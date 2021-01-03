fea3c5c3b29704c507e434dc962061774ee6af0f
hosam <hosamelmalt@gmail.com>
2021-01-03 12:28:32 +0200

    Initial Commit

---
+++ Fashion swiftui/Fashion_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_swiftuiApp.swift
+//  Fashion swiftui
+//
+//  Created by hosam on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
